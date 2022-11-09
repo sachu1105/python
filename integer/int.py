e8124926c8bdc28fac3ffad4cd207506546e3b70
sachu sajith <116347275+sachu1105@users.noreply.github.com>
2022-11-09 15:53:55 +0530

    Add files via upload

---
+++ integer/int.py
@@ -0,0 +1,29 @@
+lst=[1,3,5,7,9,11,34]
+lst1=[5,13,45,7,20,65,1]
+s=int(0)
+c=int(0)
+for i in lst and lst1:
+    if (len(lst)==len(lst)):
+        print("lists are same length")
+        break
+for i in range(0,len(lst)and len(lst)):
+    s=s+lst[i]
+    c=c+lst[i]
+for i in range(0,len(lst) and len(lst1)):
+    if (s==c):
+        print("sum of values are same")
+        break
+    else:
+        print("sum of values are different")
+        break
+    print("elements that matched are:")
+l=[]
+for i in range(0,len(lst)):
+    for j in range(0,len(lst1)):
+        if (lst[i]==lst1[j]):
+            l.append(lst[i] and lst1[j])
+        else:
+            p=i 
+            continue
+print(l)                                       
+    
